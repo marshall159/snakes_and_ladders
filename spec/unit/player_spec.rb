cda86add79b414e32bf0d109af80bcd4b84e699a
Aneel Marshall <aneel.marshall@outlook.com>
2018-12-09 20:38:53 +0000

    test: add unit tests for Player#intitalize

--- spec/unit/player_spec.rb
+++ spec/unit/player_spec.rb
@@ -1,10 +1,22 @@
 require 'player'
 
 describe Player do
-  subject(:player) { described_class.new('Aneel') }
+  subject(:player) { described_class.new }
 
   describe '#name' do
     it "returns the player's name" do
+      expect(player.name).to eq('One')
+    end
+  end
+
+  describe '#intialize' do
+    it 'creates a default name of "One"' do
+      expect(player.name).to eq('One')
+    end
+
+    it 'takes a name if given' do
+      player = Player.new('Aneel')
+
       expect(player.name).to eq('Aneel')
     end
   end
