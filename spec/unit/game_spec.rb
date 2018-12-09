bd8fa96066abe6dee31784367070488b11ad4457
Aneel Marshall <aneel.marshall@outlook.com>
2018-12-09 12:35:18 +0000

    test: unit test #intialize

---
+++ spec/unit/game_spec.rb
@@ -0,0 +1,11 @@
+require 'game'
+
+describe Game do
+  subject(:game) { described_class.new }
+
+  describe '#initialize' do
+    it 'token is at square one' do
+      expect(game.token.position).to eq(1)
+    end
+  end
+end
