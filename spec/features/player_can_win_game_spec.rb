952d0f3e1f634dd95a97a8cf4a9e5c2a7076f8cf
Aneel Marshall <aneel.marshall@outlook.com>
2018-12-09 17:29:14 +0000

    test: add feat test player_can_win_game

---
+++ spec/features/player_can_win_game_spec.rb
@@ -0,0 +1,34 @@
+# As a player
+# I want to be able to win the game
+# So that I can gloat to everyone around
+
+require 'game'
+
+describe 'Player wins' do
+
+  it 'when Player lands on square 100' do
+    given_the_game_has_started
+    when_the_token_is_on_square_97
+    and_the_token_is_moved_three_spaces
+    then_the_player_has_won
+  end
+
+  def given_the_game_has_started
+    @die = double(:die, roll: 4)
+    @game = Game.new(@die)
+  end
+
+  def when_the_token_is_on_square_97
+    24.times { @game.move }
+  end
+
+  def and_the_token_is_moved_three_spaces
+    allow(@die).to receive(:roll).and_return(3)
+
+    @result = @game.move
+  end
+
+  def then_the_player_has_won
+    expect(@result).to eq('Player has won')
+  end
+end
